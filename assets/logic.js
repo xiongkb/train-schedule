cf11a8e7903c3ade1cbd36ba36ec9ed5346d6fbc
xiongkb <mkan-bx@hotmail.com>
2020-01-14 22:07:43 -0800

    created logic to submit user submisson to firebase database

--- assets/logic.js
+++ assets/logic.js
@@ -25,19 +25,19 @@ for (var i = 0; i < 60; i = i+5) {
 }
 
 // My web app's Firebase configuration
-// var firebaseConfig = {
-//     apiKey: "AIzaSyCiJ31L3E6h7-dhI_CmRXgGN4kI-dFtBs8",
-//     authDomain: "ucb-train-schedule-hw.firebaseapp.com",
-//     databaseURL: "https://ucb-train-schedule-hw.firebaseio.com",
-//     projectId: "ucb-train-schedule-hw",
-//     storageBucket: "ucb-train-schedule-hw.appspot.com",
-//     messagingSenderId: "294459173072",
-//     appId: "1:294459173072:web:ebf2d8467e0568b46416d5",
-//     measurementId: "G-3PPGR4QKGM"
-// };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-// var data = firebase.database();
+var firebaseConfig = {
+    apiKey: "AIzaSyCiJ31L3E6h7-dhI_CmRXgGN4kI-dFtBs8",
+    authDomain: "ucb-train-schedule-hw.firebaseapp.com",
+    databaseURL: "https://ucb-train-schedule-hw.firebaseio.com",
+    projectId: "ucb-train-schedule-hw",
+    storageBucket: "ucb-train-schedule-hw.appspot.com",
+    messagingSenderId: "294459173072",
+    appId: "1:294459173072:web:ebf2d8467e0568b46416d5",
+    measurementId: "G-3PPGR4QKGM"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var data = firebase.database();
 
 $(".submit-btn").on("click", function(event) {
 event.preventDefault();
@@ -49,4 +49,14 @@ var frequency = $("#z-min").val();
 var selectedHour = $("#hour").val();
 var selectedMins = $("#min").val();
 var departTime = selectedHour + ":" + selectedMins;
+
+// upload data submisson to firebase
+var submisson = {
+    name: trainName,
+    destination: destination,
+    departure: departTime,
+    frequency :frequency
+}
+
+data.ref().push(submisson);
 })
